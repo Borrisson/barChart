cd74460ef4a0379d221acc2d6d04a8d8ab3b1953
luc borris <lucborris@gmail.com>
2020-11-18 14:26:05 +0000

    Added the x and y axis naming input option. Pretty self explanitory. Next is to add the values within the bars and add an input bar that let's the user decide what the gridScale step should be.

--- barChart.js
+++ barChart.js
@@ -12,8 +12,11 @@ const list = document.getElementsByTagName("li");
 const deleteBtns = document.getElementsByClassName("delete");
 const legendInputName = document.getElementById("legend-input");
 const legendButton = document.getElementById("legend-input-enter");
+const valueInputName = document.getElementById("value-input");
+const valueButton = document.getElementById("value-input-enter");
 const genNew = document.getElementById("genNew");
 const slider = document.getElementById("slider");
+
 
 let dataSet = {
   "Classical music": 10,
@@ -45,88 +48,94 @@ class Barchart {
     this.canvas = options.canvas;
     this.ctx = this.canvas.getContext("2d");
     this.colors = options.colors;
+  }
+  draw() {
+    let maxValue = 0;
+    for (let categ in this.options.data) {
+      maxValue = Math.max(maxValue, this.options.data[categ]);
+    }
+    let canvasActualHeight = this.canvas.height - this.options.padding * 2;
+    let canvasActualWidth = this.canvas.width - this.options.padding * 2;
+    //everytime the draw function is called it will draw to the next rounded up 5, using the slider scale.
+    slider.setAttribute("max", Math.ceil(Number(maxValue) / 5) * 5);
+    //drawing the grid lines
 
-    this.draw = function () {
-      let maxValue = 0;
-      for (let categ in this.options.data) {
-        maxValue = Math.max(maxValue, this.options.data[categ]);
-      }
-      let canvasActualHeight = this.canvas.height - this.options.padding * 2;
-      let canvasActualWidth = this.canvas.width - this.options.padding * 2;
-      slider.setAttribute("max", Math.ceil(Number(maxValue)/5)*5);
-      //drawing the grid lines
-      let gridValue = 0;
-      while (gridValue <= maxValue) {
-        let gridY = canvasActualHeight * (1 - gridValue / maxValue) + this.options.padding;
-        console.log(gridY, canvasActualHeight);
-        drawLine(
-          this.ctx,
-          0,
-          gridY,
-          this.canvas.width,
-          gridY,
-          this.options.gridColor
-        );
+    let gridValue = 0;
+    while (gridValue <= maxValue) {
+      let gridY = canvasActualHeight * (1 - gridValue / maxValue) + this.options.padding;
+      drawLine(
+        this.ctx,
+        0,
+        gridY,
+        this.canvas.width,
+        gridY,
+        this.options.gridColor
+      );
 
-        //writing grid markers
-        this.ctx.save();
-        this.ctx.fillStyle = this.options.gridColor;
-        this.ctx.textBaseline="bottom"; 
-        this.ctx.font = "bold 10px Arial";
-        this.ctx.fillText(gridValue, 10, gridY - 2);
-        this.ctx.restore();
-
-        gridValue += this.options.gridScale;
-      }
-
-      //drawing the bars
-      let barIndex = 0;
-      let numberOfBars = Object.keys(this.options.data).length;
-      let barSize = (canvasActualWidth) / numberOfBars;
-
-      for (let categ in this.options.data) {
-        let val = this.options.data[categ];
-        let barHeight = Math.round(canvasActualHeight * val / maxValue);
-        drawBar(
-          this.ctx,
-          this.options.padding + barIndex * barSize,
-          this.canvas.height - barHeight - this.options.padding,
-          barSize,
-          barHeight,
-          this.colors[barIndex % this.colors.length]
-        );
-
-        barIndex++;
-      }
-
-      //drawing series name
+      //writing grid markers
       this.ctx.save();
+      this.ctx.fillStyle = this.options.gridColor;
       this.ctx.textBaseline = "bottom";
-      this.ctx.textAlign = "center";
-      this.ctx.fillStyle = "#000000";
-      this.ctx.font = "bold 14px Arial";
-      this.ctx.fillText(this.options.seriesName, this.canvas.width / 2, this.canvas.height);
+      this.ctx.font = "bold 10px Arial";
+      this.ctx.fillText(gridValue, 10, gridY - 2);
       this.ctx.restore();
 
-      //draw legend
-      barIndex = 0;
-      let legend = document.querySelector("legend[for='myCanvas']");
-      let ul = document.createElement("ul");
-      legend.append(ul);
-      for (let categ in this.options.data) {
-        ul.append(defaultListElement(barIndex, categ, this.colors));
-        deleteBtns[barIndex].addEventListener("click", removeParent, false);
-        deleteBtns[barIndex].setAttribute("id", categ);
-        barIndex++;
-      }
-    };
-  }
+      gridValue += this.options.gridScale;
+    }
+
+    //drawing the bars
+    let barIndex = 0;
+    let numberOfBars = Object.keys(this.options.data).length;
+    let barSize = (canvasActualWidth) / numberOfBars;
+
+    for (let categ in this.options.data) {
+      let val = this.options.data[categ];
+      let barHeight = Math.round(canvasActualHeight * val / maxValue);
+      drawBar(
+        this.ctx,
+        this.options.padding + barIndex * barSize,
+        this.canvas.height - barHeight - this.options.padding,
+        barSize,
+        barHeight,
+        this.colors[barIndex % this.colors.length]
+      );
+
+      barIndex++;
+    }
+    //drawing value name
+    this.ctx.save();
+    this.ctx.font = "bold 14px Arial";
+    this.ctx.fillStyle = "#000000";
+    this.ctx.fillText(this.options.valueName, 2, 15);
+    this.ctx.restore();
+    //drawing series name
+    this.ctx.save();
+    this.ctx.textBaseline = "bottom";
+    this.ctx.textAlign = "center";
+    this.ctx.fillStyle = "#000000";
+    this.ctx.font = "bold 14px Arial";
+    this.ctx.fillText(this.options.seriesName, this.canvas.width / 2, this.canvas.height);
+    this.ctx.restore();
+
+    //draw legend
+    barIndex = 0;
+    let legend = document.querySelector("legend[for='myCanvas']");
+    let ul = document.createElement("ul");
+    legend.append(ul);
+    for (let categ in this.options.data) {
+      ul.append(defaultListElement(barIndex, categ, this.colors));
+      deleteBtns[barIndex].addEventListener("click", removeParent, false);
+      deleteBtns[barIndex].setAttribute("id", categ);
+      barIndex++;
+    }
+  };
 }
 
 let myBarchart = new Barchart(
   {
     canvas: myCanvas,
     seriesName: "Vinyl records",
+    valueName: "# of Vinyls sold",
     padding: 30,
     gridScale: 5,
     gridColor: "#000000",
@@ -171,13 +180,13 @@ function defaultListElement(index, input, color) {
 
 function freshCanvas() {
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-    let legend = document.getElementById("legend");
-    legend.removeChild(legend.childNodes[0]);
+  let legend = document.getElementById("legend");
+  legend.removeChild(legend.childNodes[0]);
 }
 
 function createListElement() {
-// /\d/.test checks wether or not there is a number in the string. No idea how it works will look into it further later. 
-// from stackoverflow "Check whether an input string contains a number in javascript."
+  // /\d/.test checks wether or not there is a number in the string. No idea how it works will look into it further later. 
+  // from stackoverflow "Check whether an input string contains a number in javascript."
   if (userInput.value.indexOf(":") !== -1 && /\d/.test(userInput.value)) {
     let splitStr = userInput.value.split(":");
     dataSet[splitStr[0].trim()] = Number(splitStr[1]);
@@ -188,29 +197,24 @@ function createListElement() {
     alert("Don't forget to write in this format <data:value> without :<> and make sure data is 'alphabetic' and value numberic.")
   }
 }
-
+//User input Data
 userButton.addEventListener("click", () => {
   if (userInput.value.length > 0) {
     createListElement();
     deleteBtns[deleteBtns.length - 1].addEventListener("click", removeParent, false);
-}});
+  }
+});
 
 userInput.addEventListener("keypress", (event) => {
   if (userInput.value.length > 0 && event.keyCode === 13) {
     createListElement();
     deleteBtns[deleteBtns.length - 1].addEventListener("click", removeParent, false);
-}});
+  }
+});
 
-legendButton.addEventListener("click", () => { 
-  if(legendInputName.value.length > 0) {
-    myBarchart.options.seriesName = legendInputName.value;
-    freshCanvas();
-    myBarchart.draw();
-    legendInputName.value = "";
-}});
-
-legendInputName.addEventListener("keypress", (event) => {
-  if(legendInputName.value.length > 0 && event.keyCode === 13) {
+//User legend Name Change
+legendButton.addEventListener("click", () => {
+  if (legendInputName.value.length > 0) {
     myBarchart.options.seriesName = legendInputName.value;
     freshCanvas();
     myBarchart.draw();
@@ -218,21 +222,49 @@ legendInputName.addEventListener("keypress", (event) => {
   }
 });
 
+legendInputName.addEventListener("keypress", (event) => {
+  if (legendInputName.value.length > 0 && event.keyCode === 13 ) {
+    myBarchart.options.seriesName = legendInputName.value;
+    freshCanvas();
+    myBarchart.draw();
+    legendInputName.value = "";
+  }
+});
+
+//User Value Name Change
+valueButton.addEventListener("click", () => {
+  if (valueInputName.value.length > 0) {
+    myBarchart.options.valueName = valueInputName.value;
+    freshCanvas();
+    myBarchart.draw();
+    valueInputName.value = "";
+  }
+});
+
+valueInputName.addEventListener("keypress", (event) => {
+  if (valueInputName.value.length > 0 && event.keyCode === 13) {
+    myBarchart.options.valueName = valueInputName.value;
+    freshCanvas();
+    myBarchart.draw();
+    valueInputName.value = "";
+  }
+});
+
 genNew.addEventListener("click", () => {
   let colors = [];
-  for(let i = 0; i < 4; i++) {
+  for (let i = 0; i < 4; i++) {
     let randomColor = "";
     do {
-    randomColor = Math.floor(Math.random()*16777215).toString(16);
-  } while(randomColor.length < 6)
-  colors.push("#" + randomColor);
+      randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    } while (randomColor.length < 6)
+    colors.push("#" + randomColor);
   }
   myBarchart.colors = colors;
   freshCanvas();
   myBarchart.draw();
 });
 
-slider.oninput = function() {
+slider.oninput = function () {
   myBarchart.options.gridScale = Number(this.value);
   freshCanvas();
   myBarchart.draw();
