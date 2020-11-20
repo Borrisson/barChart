50387037d2621ff24630fa53f54a93ec05b76332
luc borris <lucborris@gmail.com>
2020-11-20 14:51:32 +0000

    I've added jQuery to the script tag, because I've added the color selector feature for each of the drag and drop nodes. Now I just realised that if I create an additional item, it will not add a div element for the appropriate legend element. On top of that, it will also not add an event listener to the div element for the color selector feature.

--- barChart.js
+++ barChart.js
@@ -109,7 +109,6 @@ class Barchart {
     let barIndex = 0;
     let numberOfBars = Object.keys(this.options.data).length;
     let barSize = (canvasActualWidth) / numberOfBars;
-    console.log(this.colors);
     for (let categ in this.options.data) {
       let val = this.options.data[categ];
       let barHeight = Math.round(canvasActualHeight * val / maxValue);
@@ -158,7 +157,6 @@ class Barchart {
       deleteBtns[barIndex].setAttribute("id", categ);
       barIndex++;
     }
-    console.log('drawn');
   };
 }
 
@@ -172,7 +170,7 @@ let myBarchart = new Barchart(
     gridColor: "#000000",
     barValPos: "top",
     data: dataSet,
-    colors: ["#a55ca5", "#67b6c7", "#bccd7a", "#eb9743", "#000000"]
+    colors: ["#a55ca5", "#67b6c7", "#bccd7a", "#eb9743"]
   }
 );
 
@@ -428,3 +426,97 @@ document.addEventListener('DOMContentLoaded', (event) => {
   });
 
 });
+
+PICKER = {
+  mouse_inside: false,
+
+  to_hex: function (dec) {
+      hex = dec.toString(16);
+      return hex.length == 2 ? hex : '0' + hex;
+  },
+
+  show: function () {
+      var input = $(this);
+      var position = input.offset();
+
+      PICKER.$colors  = $('<canvas width="230" height="150" ></canvas>');
+      PICKER.$colors.css({
+          'position': 'absolute',
+          'top': position.top + input.height() + 9,
+          'left': position.left,
+          'cursor': 'crosshair',
+          'display': 'none'
+      });
+      $('body').append(PICKER.$colors.fadeIn());
+      PICKER.colorctx = PICKER.$colors[0].getContext('2d');
+
+      PICKER.render();
+
+      PICKER.$colors
+          .click(function (e) {
+              var new_color = PICKER.get_color(e);
+              $(input).css({'background-color': new_color}).val(new_color).trigger('change').removeClass('color-picker-binded');
+              PICKER.close();
+              var x = $(input).index();
+              myBarchart.colors[x] = new_color;
+              freshCanvas();
+              myBarchart.draw();
+          })
+          .hover(function () {
+              PICKER.mouse_inside=true;
+          }, function () {
+              PICKER.mouse_inside=false;
+          });
+
+      $("body").mouseup(function () {
+          if (!PICKER.mouse_is_inside) PICKER.close();
+      });
+  },
+
+  bind_inputs: function () {
+      $('div[draggable="true"]').each(function () {
+          $(this).click(PICKER.show);
+      }).addClass('color-picker-binded');
+  },
+
+  close: function () {PICKER.$colors.fadeOut(PICKER.$colors.remove);},
+
+  get_color: function (e) {
+      var pos_x = e.pageX - PICKER.$colors.offset().left;
+      var pos_y = e.pageY - PICKER.$colors.offset().top;
+
+      data = PICKER.colorctx.getImageData(pos_x, pos_y, 1, 1).data;
+      return '#' + PICKER.to_hex(data[0]) + PICKER.to_hex(data[1]) + PICKER.to_hex(data[2]);
+  },
+
+// Build Color palette
+  render: function () {
+      var gradient = PICKER.colorctx.createLinearGradient(0, 0, PICKER.$colors.width(), 0);
+
+      // Create color gradient
+      gradient.addColorStop(0,    "rgb(255,   0,   0)");
+      gradient.addColorStop(0.15, "rgb(255,   0, 255)");
+      gradient.addColorStop(0.33, "rgb(0,     0, 255)");
+      gradient.addColorStop(0.49, "rgb(0,   255, 255)");
+      gradient.addColorStop(0.67, "rgb(0,   255,   0)");
+      gradient.addColorStop(0.84, "rgb(255, 255,   0)");
+      gradient.addColorStop(1,    "rgb(255,   0,   0)");
+
+      // Apply gradient to canvas
+      PICKER.colorctx.fillStyle = gradient;
+      PICKER.colorctx.fillRect(0, 0, PICKER.colorctx.canvas.width, PICKER.colorctx.canvas.height);
+
+      // Create semi transparent gradient (white -> trans. -> black)
+      gradient = PICKER.colorctx.createLinearGradient(0, 0, 0, PICKER.$colors.height());
+      gradient.addColorStop(0,   "rgba(255, 255, 255, 1)");
+      gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
+      gradient.addColorStop(0.5, "rgba(0,     0,   0, 0)");
+      gradient.addColorStop(1,   "rgba(0,     0,   0, 1)");
+
+      // Apply gradient to canvas
+      PICKER.colorctx.fillStyle = gradient;
+      PICKER.colorctx.fillRect(0, 0, PICKER.colorctx.canvas.width, PICKER.colorctx.canvas.height);
+  }
+};
+
+PICKER.bind_inputs();
